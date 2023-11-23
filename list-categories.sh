e47721de32d7122e8d10cedcc30f4f98ac97d5b9
Aleksey Saprunov <sav@altlinux.org>
2023-11-23 15:50:38 +0400

    fix: fix list script and spec file

--- list-categories.sh
+++ list-categories.sh
@@ -3,12 +3,9 @@
 old_categories_dir="/usr/share/alterator/desktop-directories"
 new_categories_dir="/usr/share/alterator/objects"
 
-find $old_categories_dir -type f -exec \
-	sed -e "s/X-Alterator-Category\s*=\(.*\)/\1/p" \;
+sed -n -e "s/X-Alterator-Category\s*=\(.*\)/\1/p" $old_categories_dir/*
 
-alterator_files=$(find $new_categories_dir -name "*.alterator")
 
-for file in "$alterator_files"; do
-	grep -q -e "^\s*Type\s*=\s*Category" $file &&
-		sed -n -e "s/s*Name\s*=\(.*\)/\1/p" $file | xargs
-done
+grep -s -l -e "\[Alterator Entry\]" $new_categories_dir/*.alterator |
+    xargs grep -l -e "Type\s*=\s*Category" |
+    xargs sed -n -e "s/Name\s*=\s*\(.*\)/\1/p"
